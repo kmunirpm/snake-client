d08d20ff83133c00699ca86ee9a8bc43992a9d0b
kmunirpm <km092009@gmail.com>
2021-11-03 23:42:57 +0000

    3rd commit

--- client.js
+++ client.js
@@ -11,13 +11,22 @@ connection.on("connect", ()=> {
   connection.setEncoding("utf8");
   console.log("connected")
   connection.write("Name: KLM",)
-});
+//  setTimeout(()=>{connection.write("Name: HEl",)}, 500)
+//  setTimeout(()=>{connection.write("Move: down",)}, 50)
+  // setInterval(() => {
+  //   connection.write("Move: left",) 
+  // }, 50)
+  // setTimeout(()=>{connection.write("Move: down",)}, 50)
 
+ 
 connection.on('data', (data) => {
   console.log(data.toString());
   connection.end();
 });
+});
 
-setTimeout(()=>{
-  connection.end();
-},1000)
+
+
+ setTimeout(()=>{
+   connection.end();
+ },8000)
